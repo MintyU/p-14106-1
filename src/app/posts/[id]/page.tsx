90033860530398f748b81242d55ba706646fbad6
MintyU <mintyu.dev@gmail.com>
2025-12-12 10:29:07 +0900

    Lecture 37

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,153 +1,257 @@
 "use client";
 
 import { apiFetch } from "@/lib/backend/client";
-import { PostCommentDto, PostWithContentDto } from "@/type/post";
-import { useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
-import { useParams } from "next/navigation";
+import type { PostCommentDto, PostWithContentDto } from "@/type/post";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { use, useEffect, useState } from "react";
 
-export default function Page() {
-  const router = useRouter();
-  const { id } = useParams<{ id: string }>();
-
+function usePost(id: number) {
   const [post, setPost] = useState<PostWithContentDto | null>(null);
-  const [postComments, setPostComments] = useState<PostCommentDto[] | null>(null);
-
-  const deletePost = (id: number) => {
-    apiFetch(`/api/v1/posts/${id}`, {
-      method: "DELETE",
-    })
-      .then((data) => {
-        alert(data.msg);
-        router.replace("/posts");
-      });
-  };
-
-  const deletePostComment = (id: number, commentId: number) => {
-    apiFetch(`/api/v1/posts/${id}/comments/${commentId}`, {
-      method: "DELETE",
-    })
-      .then((data) => {
-        alert(data.msg);
-
-        if (postComments == null) return;
-
-        setPostComments(postComments.filter((comment) => comment.id != commentId));
-      });
-  };
-
-  const writePostComment = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const form = e.target as HTMLFormElement;
-    const content = form.elements.namedItem("content") as HTMLTextAreaElement;
-
-    content.value = content.value.trim();
-
-    if (content.value.length === 0) {
-      alert("댓글 내용을 입력해주세요.");
-      content.focus();
-      return;
-    }
-
-    if (content.value.length < 2) {
-      alert("댓글 내용을 2자 이상 입력해주세요.");
-      content.focus();
-      return;
-    }
-
-    apiFetch(`/api/v1/posts/${id}/comments`, {
-      method: "POST",
-      body: JSON.stringify({ content: content.value }),
-    })
-      .then((data) => {
-
-        alert(data.msg);
-        content.value = "";
-
-        if (postComments == null) return;
-
-        setPostComments([...postComments, data.data]);
-        content.focus();
-      });
-  };
 
   useEffect(() => {
     apiFetch(`/api/v1/posts/${id}`)
       .then(setPost)
-      .catch((e) => {
-        alert(`${e.resultCode} : ${e.msg}`);
-      });
-
-    apiFetch(`/api/v1/posts/${id}/comments`)
-      .then(setPostComments)
-      .catch((e) => {
-        alert(`${e.resultCode} : ${e.msg}`);
+      .catch((error) => {
+        alert(`${error.resultCode} : ${error.msg}`);
       });
   }, []);
 
-  if (post == null) {
-    return <div>로딩중...</div>;
-  }
+  const deletePost = (id: number, onSuccess: () => void) => {
+    apiFetch(`/api/v1/posts/${id}`, {
+      method: "DELETE",
+    }).then(onSuccess);
+  };
+
+  return {
+    post,
+    deletePost,
+  };
+}
+
+function usePostComments(id: number) {
+  const [postComments, setPostComments] = useState<PostCommentDto[] | null>(
+    null
+  );
+
+  useEffect(() => {
+    apiFetch(`/api/v1/posts/${id}/comments`)
+      .then(setPostComments)
+      .catch((error) => {
+        alert(`${error.resultCode} : ${error.msg}`);
+      });
+  }, []);
+
+  const deleteComment = (
+    id: number,
+    commentId: number,
+    onSuccess: (data: any) => void
+  ) => {
+    apiFetch(`/api/v1/posts/${id}/comments/${commentId}`, {
+      method: "DELETE",
+    }).then((data) => {
+      if (postComments == null) return;
+
+      setPostComments(postComments.filter((c) => c.id != commentId));
+
+      onSuccess(data);
+    });
+  };
+
+  const writeComment = (
+    id: number,
+    content: string,
+    onSuccess: (data: any) => void
+  ) => {
+    apiFetch(`/api/v1/posts/${id}/comments`, {
+      method: "POST",
+      body: JSON.stringify({
+        content,
+      }),
+    }).then((data) => {
+      if (postComments == null) return;
+
+      setPostComments([...postComments, data.data]);
+
+      onSuccess(data);
+    });
+  };
+
+  return {
+    postComments,
+    deleteComment,
+    writeComment,
+  };
+}
+
+function PostInfo({ postState }: { postState: ReturnType<typeof usePost> }) {
+  const router = useRouter();
+  const { post, deletePost: _deletePost } = postState;
+
+  if (post == null) return <div>로딩중...</div>;
+
+  const deletePost = () => {
+    if (!confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`)) return;
+
+    _deletePost(post.id, () => {
+      router.replace("/posts");
+    });
+  };
 
   return (
     <>
-      <h1>글 상세페이지</h1>
-
       <div>번호 : {post.id}</div>
       <div>제목: {post.title}</div>
       <div style={{ whiteSpace: "pre-line" }}>{post.content}</div>
 
       <div className="flex gap-2">
-        <button className="bg-white text-red-500 border border-red-500 p-2 rounded hover:bg-red-100" onClick={() => confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) && deletePost(post.id)}>
+        <button className="p-2 rounded border" onClick={deletePost}>
           삭제
         </button>
-        <Link href={`/posts/${post.id}/edit`} className="bg-white text-blue-500 border border-blue-500 p-2 rounded hover:bg-blue-100">
+        <Link className="p-2 rounded border" href={`/posts/${post.id}/edit`}>
           수정
         </Link>
       </div>
+    </>
+  );
+}
 
+function PostCommentWrite({
+  id,
+  postCommentsState,
+}: {
+  id: number;
+  postCommentsState: ReturnType<typeof usePostComments>;
+}) {
+  const { writeComment } = postCommentsState;
+
+  const handleCommentWriteFormSubmit = (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+
+    const contentTextarea = form.elements.namedItem(
+      "content"
+    ) as HTMLTextAreaElement;
+
+    contentTextarea.value = contentTextarea.value.trim();
+
+    if (contentTextarea.value.length === 0) {
+      alert("댓글 내용을 입력해주세요.");
+      contentTextarea.focus();
+      return;
+    }
+
+    if (contentTextarea.value.length < 2) {
+      alert("댓글 내용을 2자 이상 입력해주세요.");
+      contentTextarea.focus();
+      return;
+    }
+
+    writeComment(id, contentTextarea.value, (data) => {
+      alert(data.msg);
+      contentTextarea.value = "";
+    });
+  };
+
+  return (
+    <>
       <h2>댓글 작성</h2>
 
-      <form onSubmit={writePostComment} className="flex gap-2 border-b border-gray-300 pb-5">
+      <form className="p-2" onSubmit={handleCommentWriteFormSubmit}>
         <textarea
-          className="border border-gray-300 rounded p-2 w-full"
+          className="border p-2 rounded"
           name="content"
-          rows={5}
+          placeholder="댓글 내용"
           maxLength={100}
-          placeholder="댓글 내용을 입력해주세요."
+          rows={5}
         />
-        <button type="submit" className="bg-white text-blue-500 border border-blue-500 p-2 rounded hover:bg-blue-100 w-24">
+        <button className="p-2 rounded border" type="submit">
           작성
         </button>
       </form>
+    </>
+  );
+}
 
+function PostCommentList({
+  id,
+  postCommentsState,
+}: {
+  id: number;
+  postCommentsState: ReturnType<typeof usePostComments>;
+}) {
+  const { postComments, deleteComment: _deleteComment } = postCommentsState;
+
+  const deleteComment = (commentId: number) => {
+    if (!confirm(`${commentId}번 댓글을 정말로 삭제하시겠습니까?`)) return;
+
+    _deleteComment(id, commentId, (data) => {
+      alert(data.msg);
+    });
+  };
+
+  if (postComments == null) return <div>로딩중...</div>;
+
+  return (
+    <>
       <h2>댓글 목록</h2>
 
-      {postComments == null && <div>댓글 로딩중...</div>}
-
-      {postComments != null && postComments.length == 0 && <div>댓글이 없습니다.</div>}
+      {postComments != null && postComments.length == 0 && (
+        <div>댓글이 없습니다.</div>
+      )}
 
       {postComments != null && postComments.length > 0 && (
-        <ul className="flex flex-col gap-2">
+        <ul>
           {postComments.map((comment) => (
-            <li key={comment.id} className="flex gap-2 items-center justify-between">
-              <div>
-                {comment.id} : {comment.content}
-              </div>
+            <li key={comment.id}>
+              {comment.id} : {comment.content}
               <button
-                className="bg-white text-red-500 border border-red-500 p-2 rounded hover:bg-red-100"
-                onClick={() =>
-                  confirm(`${comment.id}번 댓글을 정말로 삭제하시겠습니까?`) &&
-                  deletePostComment(post.id, comment.id)
-                }>
+                className="p-2 rounded border"
+                onClick={() => deleteComment(comment.id)}
+              >
                 삭제
               </button>
             </li>
           ))}
         </ul>
       )}
+    </>
+  );
+}
+
+function PostCommentWriteAndList({
+  id,
+  postCommentsState,
+}: {
+  id: number;
+  postCommentsState: ReturnType<typeof usePostComments>;
+}) {
+  return (
+    <>
+      <PostCommentWrite id={id} postCommentsState={postCommentsState} />
+
+      <PostCommentList id={id} postCommentsState={postCommentsState} />
+    </>
+  );
+}
+
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id: idStr } = use(params);
+  const id = parseInt(idStr);
+
+  const postState = usePost(id);
+  const postCommentsState = usePostComments(id);
+
+  return (
+    <>
+      <h1>글 상세페이지</h1>
+
+      <PostInfo postState={postState} />
+
+      <PostCommentWriteAndList id={id} postCommentsState={postCommentsState} />
     </>
   );
 }
